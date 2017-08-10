da2cf94fe02edde7de79fdb9180d5fc7a7511a78
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2017-08-10 05:03:16 -0300

    Add component SpanLink first structure

---
+++ src/components/SpanLink/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'enzyme'
+import { expect } from 'chai'
+
+
+describe('<SpanLink />', () => {
+  const wrapper = render()
+  it('render component without crash', () => {
+    
+  })
+})
