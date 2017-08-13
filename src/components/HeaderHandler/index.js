a9739265cf63bd07bfa934344b3b7cd2068444e0
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2017-08-13 12:32:44 -0300

    Add logic of rendering props

--- src/components/HeaderHandler/index.js
+++ src/components/HeaderHandler/index.js
@@ -3,14 +3,18 @@ import React from 'react'
 import Form from '../Form'
 import Hash from '../Hash'
 
-const HeaderHandler = () => (
+const HashesResult = (hashes) => (
+  hashes.map((hash) => <Hash text={hash} />)
+)
+
+const HeaderHandler = props => (
   <div className="header__handler">
     <div className="header__handler__text">
       insert your hashtag here
     </div>
-    <Form />
+    <Form handleSubmit={props.handleSubmit} />
     <div className="header__handler__hashes">
-      <Hash text="LoveGolden" />
+      {HashesResult(props.hashes)}
     </div>
   </div>
 )
