a47191781e01bf1afd5b796614242d2099dc109e
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2017-08-12 23:33:01 -0300

    Add tests in the form component

---
+++ src/components/Form/index.test.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { render } from 'enzyme'
+import { expect } from 'chai'
+import Form from '.'
+
+describe('<Form />', () => {
+  const primProps = {
+    handleChange: () => '',
+    handleSubmit: () => ''
+  }
+  const wrapper = render(<Form {...primProps} />)
+  it('should component exist', () => {
+    expect(wrapper).to.exist
+  })
+  it('render component without crash', () => {
+    expect(wrapper.length).to.be.equal(1)
+  })
+  it('did the handleChange property exists on the proptype of the component?', () => {
+    expect(NewsForm.propTypes).to.have.a.property('handleChange')
+  })
+  it('did the handleSubmit property exists on the proptype of the component?', () => {
+    expect(NewsForm.propTypes).to.have.a.property('handleSubmit')
+  })
+  it('looking for a form tag', () => {
+    expect(wrapper.find('form')).to.exist
+  })
+  it('should form have a submit button', () => {
+    expect(wrapper.find('button').attr().type).to.be.equal('submit')
+  })
+})
