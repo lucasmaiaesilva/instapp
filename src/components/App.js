acbba98c70ea0a6302e07e44a187b90cfc420a04
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2017-08-14 23:10:00 -0300

    Add logic of remove hashes

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react'
 import Hash from './Hash'
 import Header from './Header'
 import Content from './Content'
+import result from '../data'
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
-      hashes: ['GaloDoido', 'LoveGolden'],
-      result: [],
+      hashes: ['snow', 'findChuckNorris'],
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.closeHash = this.closeHash.bind(this)
@@ -19,9 +19,12 @@ class App extends Component {
     console.log('submited')
   }
   closeHash (e) {
-    console.log(e.target.id)
+    let arrHashes = this.state.hashes
+    arrHashes.splice(e.target.id, 1)
+    this.setState({ hashes: arrHashes })
   }
   render () {
+    // console.log(result.data.map((item) => item.images.standard_resolution.url))
     return (
       <div className="instapp">
         <Header
---
+++ src/data/index.js
@@ -0,0 +1,78 @@
+export default {
+  "data": [{
+    "type": "image",
+    "users_in_photo": [],
+    "filter": "Earlybird",
+    "tags": ["snow"],
+    "comments": {
+      "count": 3
+    },
+    "caption": {
+      "created_time": "1296703540",
+      "text": "#Snow",
+      "from": {
+        "username": "emohatch",
+        "id": "1242695"
+      },
+      "id": "26589964"
+    },
+    "likes": {
+      "count": 1
+    },
+    "link": "http://instagr.am/p/BWl6P/",
+    "user": {
+      "username": "emohatch",
+      "profile_picture": "http://distillery.s3.amazonaws.com/profiles/profile_1242695_75sq_1293915800.jpg",
+      "id": "1242695",
+      "full_name": "Dave"
+    },
+    "created_time": "1296703536",
+    "images": {
+      "standard_resolution": {
+        "url": "https://www.surfnetkids.com/resources/wp-content/uploads/2016/12/snow-720x477.jpg",
+        "width": 612,
+        "height": 612
+      }
+    },
+    "id": "22699663",
+    "location": null
+  },
+  {
+    "type": "image",
+    "users_in_photo": [],
+    "filter": "Earlybird",
+    "tags": ["snow"],
+    "comments": {
+      "count": 3
+    },
+    "caption": {
+      "created_time": "1296703540",
+      "text": "#Snow",
+      "from": {
+        "username": "emohatch",
+        "id": "1242695"
+      },
+      "id": "26589964"
+    },
+    "likes": {
+      "count": 1
+    },
+    "link": "http://instagr.am/p/BWl6P/",
+    "user": {
+      "username": "emohatch",
+      "profile_picture": "http://distillery.s3.amazonaws.com/profiles/profile_1242695_75sq_1293915800.jpg",
+      "id": "1242695",
+      "full_name": "Dave"
+    },
+    "created_time": "1296703536",
+    "images": {
+      "standard_resolution": {
+        "url": "https://media1.giphy.com/media/Yy26NRbpB9lDi/200_s.gif",
+        "width": 612,
+        "height": 612
+      }
+    },
+    "id": "22699663",
+    "location": null
+  }]
+}
